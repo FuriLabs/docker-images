7c49ccf8da95466acb89c56334a5858f6eae691b
Bardia Moshiri <fakeshell@bardia.tech>
2024-03-09 15:03:39 -0500

    repo-furios-sign: export GPG_TTY to not error out during signing
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- extra/repo-furios-sign.sh
+++ extra/repo-furios-sign.sh
@@ -7,5 +7,7 @@ set -e
 
 wget -q "${GPG_STAGINGPRODUCTION_SIGNING_KEY}"
 
+export GPG_TTY=$(tty)
+
 cat $(basename "${GPG_STAGINGPRODUCTION_SIGNING_KEY}") | gpg --import --batch
 exec debsign -k97E5FB897E9B94B4E3ED94966261FCC7EE607820 *.changes
